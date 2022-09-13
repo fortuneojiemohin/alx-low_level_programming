f1f8f9e1837aa2dfc6f79180900acd815a4fbfac
fortuneojiemohin <fosco4u@gmail.com>
2022-09-12 19:56:30 -0700

    my 5 works

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+
+ * main - prints using puts
+
+ * Description - prints a line using the puts function
+
+ * Return: returns 0
+
+ */
+
+int main(void)
+  
+{ puts("\"Programming is like building a multilingual puzzle");
+  
+  return (0); }
