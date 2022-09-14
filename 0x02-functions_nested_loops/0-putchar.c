31efd72d8e1e7160447df0255a04a78ca2ce9701
fortuneojiemohin <fosco4u@gmail.com>
2022-09-13 19:41:08 -0700

    my 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,33 +1,20 @@
 #include "main.h"
 
 /**
-
- * main - Prints _putchar
-
- *
-
- * Return: Always 0 (Success)
-
- */
+* main - Prints _putchar
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
   
 {
-  
-  char abe[] = "_putchar";
-  
-  int c;
-  
-  for (c = 0; c < 8; c++)
-    
-    {
-      
-      _putchar(abe[c]);
-      
-    }
-  
-  _putchar('\n');
-  
-  return (0);
-  
+char abe[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++) 
+{     
+_putchar(abe[c]);
+}
+_putchar('\n');
+return (0);
 }
