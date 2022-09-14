3cfb27041bb345616225102034739c5ff5d4dc16
fortuneojiemohin <fosco4u@gmail.com>
2022-09-13 19:43:38 -0700

    my 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,12 @@
 * Return: Always 0 (Success)
 */
 
-int main(void)
-  
+int main(void) 
 {
 char abe[] = "_putchar";
 int c;
-for (c = 0; c < 8; c++) 
-{     
+ for (c = 0; c < 8; c++)
+{
 _putchar(abe[c]);
 }
 _putchar('\n');
