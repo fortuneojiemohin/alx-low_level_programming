2d312057a941c16836ec0abaf6ea5ccaf7ca93bb
fortuneojiemohin <fosco4u@gmail.com>
2022-09-19 21:34:55 -0700

    my 9 works

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+#include "main.h"
+
+/**
+* _puts - Prints a string
+* @str: The string to print
+*
+* Return: void
+*/
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str + 0);
+++str;
+}
+_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+* print_rev - Prints a string in reverse
+* @s: The string to print
+*
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+int index;
+/**
+* finds the length of string without null character
+*/
+for (index = 0; s[index] != '\0'; ++index)
+/**
+* print char from the last index as you decrement
+*/
+for (--index; index >= 0; --index)
+_putchar(s[index]);
+_putchar('\n');
+}
